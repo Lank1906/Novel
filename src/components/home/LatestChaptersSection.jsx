575f9bdb42d47941eb252394543fb9846d8bb33a
Lanhh Gaminggg <133671984+Lank1906@users.noreply.github.com>
2025-10-20 10:00:00 +0700

    add: lastest chapter section

---
+++ src/components/home/LatestChaptersSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LatestChaptersSection = () => {
+  return (
+    <section className="latest-chapters">
+      <h2>Chapter mới nhất</h2>
+      <ul className="chapter-list">
+        <li>Truyện A - Chapter 12</li>
+        <li>Truyện B - Chapter 8</li>
+        <li>Truyện C - Chapter 5</li>
+      </ul>
+    </section>
+  );
+};
+
+export default LatestChaptersSection;
